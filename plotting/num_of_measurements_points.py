53f182b87e058ccd94eac26d2ca39d790247117e
MuhammadVT <mamatripik@gmail.com>
2017-08-03 13:03:45 -0400

    add a function to plot the data by IMF clock angle

--- plotting/num_of_measurements_points.py
+++ plotting/num_of_measurements_points.py
@@ -1,4 +1,5 @@
-def num_plot(ax, data_dict, season, cmap, bounds, lat_min=50):
+def num_plot(ax, data_dict, season, cmap, bounds,
+             lat_min=50, title="xxx"):
     
     """ plots the flow vectors in MLT coords """
 
@@ -49,7 +50,7 @@ def num_plot(ax, data_dict, season, cmap, bounds, lat_min=50):
                     ,cmap=cmap,norm=norm)
 
     # add labels
-    ax.set_title("Number of Measurements, " + season[0].upper()+season[1:] + ", Kp<3", fontsize="small")
+    ax.set_title(title, fontsize="small")
     # add latitudinal labels
     fnts = 'x-small'
     ax.annotate("80", xy=(0, -10), ha="left", va="bottom", fontsize=fnts)
@@ -85,7 +86,7 @@ def add_cbar(fig, coll, bounds, label="Number of Measurements", cax=None):
 
     return
 
-def main():
+def by_season():
 
     import datetime as dt
     import matplotlib.pyplot as plt
@@ -116,7 +117,6 @@ def main():
     # build a custom color map and bounds
     color_list = ['purple', 'b', 'dodgerblue', 'c', 'g', 'y', 'orange', 'r']
     cmap = mpl.colors.ListedColormap(color_list)
-    #bounds = range(250, 2250, 250)
     bounds = range(0, 4000, 500)
     bounds[0] = 300
     bounds.append(10000)
@@ -150,5 +150,81 @@ def main():
 
     return fig
 
+def by_imf_clock_angle():
+
+    import datetime as dt
+    import matplotlib.pyplot as plt
+    from convection import fetch_data
+    import matplotlib as mpl
+
+    # input parameters
+    #nvel_min=300
+    nvel_min=100
+    lat_range=[52, 59]
+    #lat_range=[40, 60]
+    #lat_min = 43
+    lat_min = 50
+
+    ftype = "fitacf"
+    #ftype = "fitex"
+
+    #seasons = ["winter", "summer", "equinox"]
+    seasons = ["winter"]
+   
+    # build a custom color map and bounds
+    color_list = ['purple', 'b', 'dodgerblue', 'c', 'g', 'y', 'orange', 'r']
+    cmap = mpl.colors.ListedColormap(color_list)
+    bounds = range(100, 500, 50)
+    #bounds[0] = 100
+    bounds.append(10000)
+
+        
+    # imf clock angle bins
+    #bins = [[65, 115], [245, 295]] 
+    bins = [[300, 60], [120, 240]]
+    bins_txt = ["Bz+", "Bz-"]
+
+    for season in seasons:
+
+        # create subplots
+        fig, axes = plt.subplots(nrows=len(bins), ncols=1, figsize=(6,8))
+        fig.subplots_adjust(hspace=0.3)
+        if len(bins) == 1:
+            axes = [axes]
+
+        fig_dir = "./plots/cosfit_plot/kp_l_3/data_in_mlt/binned_by_imf_clock_angle/"
+        fig_name = season + "_binned_by_imf_Bz_num_of_points_" + str(lat_range[0]) +"_to_lat" + str(lat_range[1])
+
+        # fetches the data from db 
+        baseLocation="../data/sqlite3/" + season + '/kp_l_3/data_in_mlt/binned_by_imf_clock_angle/'
+        
+        # loop through the bins
+        for i, bn in enumerate(bins):
+            dbName = "imf_clock_angle_" + str(bn[0]) + "_to_" + str(bn[1]) +\
+                        "_" + ftype + ".sqlite"
+
+            data_dict = fetch_data(ftype=ftype, nvel_min=nvel_min, 
+                            lat_range=lat_range, baseLocation=baseLocation,
+                            dbName=dbName)
+
+            # plot the flow vectors
+            title = "Number of Measurements, " + season[0].upper()+season[1:] +\
+                    r", Kp $\leq$ 2+" + ", IMF " + bins_txt[i]
+            coll = num_plot(axes[i], data_dict, season, cmap, bounds,
+                            lat_min=lat_min, title=title)
+
+        # add colorbar
+        fig.subplots_adjust(right=0.78)
+        cbar_ax = fig.add_axes([0.83, 0.25, 0.02, 0.5])
+        add_cbar(fig, coll, bounds, cax=cbar_ax, label="Number of Measurements")
+
+        # save the fig
+        #fig.savefig(fig_dir + fig_name + ".png", dpi=300)
+        plt.show()
+
+        return
+
+
 if __name__ == "__main__":
-    main()
+    #by_season()
+    by_imf_clock_angle()
